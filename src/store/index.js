3afa4a192726f448d16aaa6a4baf216082dc3bcf
mark-drastrup <mark.drastrup@gmail.com>
2019-12-14 15:57:45 +0900

    Added todo and in progress columns with drag and drop functionality

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,33 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    todos: [
+      { id: 1, text: "Walk the dog" },
+      { id: 2, text: "Do the dishes" },
+      { id: 3, text: "Study coding" }
+    ],
+    inProgress: [{ id: 4, text: "Clean room" }]
   },
   mutations: {
+    updateTodo(state, array) {
+      state.todos = array;
+      console.log(state.todos);
+    },
+    updateInProgress(state, array) {
+      state.inProgress = array;
+    }
   },
   actions: {
+    updateTodo({ commit }, array) {
+      commit("updateTodo", array);
+    },
+    updateInProgress({ commit }, array) {
+      commit("updateInProgress", array);
+    }
   },
-  modules: {
-  }
-})
+  modules: {}
+});
--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,5 +1,40 @@
 <template>
   <div class="board">
-    <h1>This is board</h1>
+    <h2>Todo</h2>
+    <draggable v-model="todos" group="tasks">
+      <div v-for="todo in todos" :key="todo.id">{{todo.text}}</div>
+    </draggable>
+
+    <h2>In Progress</h2>
+    <draggable v-model="inProgress" group="tasks">
+      <div v-for="todo in inProgress" :key="todo.id">{{todo.text}}</div>
+    </draggable>
   </div>
 </template>
+
+<script>
+import draggable from "vuedraggable";
+export default {
+  components: {
+    draggable
+  },
+  computed: {
+    todos: {
+      get() {
+        return this.$store.state.todos;
+      },
+      set(value) {
+        this.$store.dispatch("updateTodo", value);
+      }
+    },
+    inProgress: {
+      get() {
+        return this.$store.state.inProgress;
+      },
+      set(value) {
+        this.$store.dispatch("updateInProgress", value);
+      }
+    }
+  }
+};
+</script>
